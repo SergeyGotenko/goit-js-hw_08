c57b68f134ff1b3c65660144b182357ebf988f1d
SergeyGotenko <gotenkosergey@gmail.com>
2022-02-19 11:48:34 +0200

    done task 2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const player = new Player('vimeo-player')
+const storageKey = localStorage.getItem(STORAGE_KEY);
+if (storageKey) {
+  player.setCurrentTime(storageKey);
+}
+
+player.on('timeupdate',throttle(data => {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+  }, 1000)
+);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import debounce from 'lodash.debounce';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', debounce(onFormInput, 500));
+
+
+initForm();
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+ };
+
+
+
+function onFormInput(event) {
+    let formData = localStorage.getItem('STORAGE_KEY');
+
+    formData = formData ? JSON.parse(formData) : {};
+    formData[event.target.name] = event.target.value;
+
+    if (!formData) return;
+
+    localStorage.setItem('STORAGE_KEY', JSON.stringify(formData));
+
+    console.log(formData)
+};
+
+function initForm() {
+    let saveMessage = localStorage.getItem('STORAGE_KEY');
+
+    if (saveMessage) {
+        saveMessage = JSON.parse(saveMessage);
+    }
+
+    Object.entries(saveMessage).forEach(
+        ([name, value]) => (form.elements[name].value = value),
+    );
+};
 
