8a7908567bda49d8f4392d1949924f4cb921df86
SergeyGotenko <gotenkosergey@gmail.com>
2022-02-20 20:59:00 +0200

    fix task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,27 +12,27 @@ initForm();
 
 function onFormSubmit(event) {
     event.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    
     event.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
  };
 
-
-
 function onFormInput(event) {
-    let formData = localStorage.getItem('STORAGE_KEY');
+    let formData = localStorage.getItem(STORAGE_KEY);
 
     formData = formData ? JSON.parse(formData) : {};
     formData[event.target.name] = event.target.value;
 
     if (!formData) return;
 
-    localStorage.setItem('STORAGE_KEY', JSON.stringify(formData));
-
-    console.log(formData)
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
+
 function initForm() {
-    let saveMessage = localStorage.getItem('STORAGE_KEY');
+    let saveMessage = localStorage.getItem(STORAGE_KEY);
 
     if (saveMessage) {
         saveMessage = JSON.parse(saveMessage);
